334cce44dd3915dcd1315c9b68c67e980a764b8c
Phi <pgh30@scarletmail.rutgers.edu>
2022-07-15 04:37:40 -0400

    added framer motion, added transitions and smooth hover

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,48 +3,69 @@ import React from 'react';
 import '../components/Card.css'
 import { mbti } from "../data/types";
 import { useMediaQuery } from 'react-responsive'
-
+import { useState } from 'react';
+import { AnimatePresence, motion } from "framer-motion"
 
 const Card = (props) => {
 
 	const pName = props.pName;
 	const pType = props.pType;
 	const isMobile = useMediaQuery({ query: '(max-width: 425px)' });
-	
+	const [isShown, setIsShown] = useState(false);
+
+	const cardSummary = mbti.filter(mbti => mbti.type.includes(pType)).map(mbti => (
+		<>
+			<div className='grid-item grid-item-person'>{pName}</div>
+			<div className='grid-item grid-item-type'>{pType}</div>
+			<div className='grid-item grid-ego-stack'>
+				<div className='grid-ego-item grid-item-hero'>{mbti.stack.hero}</div>
+				<div className='grid-ego-item grid-item-parent'>{mbti.stack.parent}</div>
+				<div className='grid-ego-item grid-item-child'>{mbti.stack.child}</div>
+				<div className='grid-ego-item grid-item-inferior'>{mbti.stack.inferior}</div>
+			</div>
+		{!isMobile && 
+			<div className='grid-item grid-shadow-stack'>
+				<div className='grid-shadow-item grid-item-nemesis'>{mbti.stack.nemesis}</div>
+				<div className='grid-shadow-item grid-item-critic'>{mbti.stack.critic}</div>
+				<div className='grid-shadow-item grid-item-trickster'>{mbti.stack.trickster}</div>
+				<div className='grid-shadow-item grid-item-demon'>{mbti.stack.demon}</div>
+			</div>
+		}
+			<div className='grid-item grid-item-quadra'>{mbti.binaries.quadra}</div>
+			
+		</>
+	));
+
 	return (
 	<>
-		<div className='card-container'>
-			<div className='card-grid-summary'>
-				{mbti.filter(mbti => mbti.type.includes(pType)).map(mbti => (
-					<>
-						<div className='grid-item grid-item-person'>{pName}</div>
-						<div className='grid-item grid-item-type'>{pType}</div>
-						<div className='grid-item grid-ego-stack'>
-							<div className='grid-ego-item grid-item-hero'>{mbti.stack.hero}</div>
-							<div className='grid-ego-item grid-item-parent'>{mbti.stack.parent}</div>
-							<div className='grid-ego-item grid-item-child'>{mbti.stack.child}</div>
-							<div className='grid-ego-item grid-item-inferior'>{mbti.stack.inferior}</div>
-						</div>
-					{!isMobile && 
-						<div className='grid-item grid-shadow-stack'>
-							<div className='grid-shadow-item grid-item-nemesis'>{mbti.stack.nemesis}</div>
-							<div className='grid-shadow-item grid-item-critic'>{mbti.stack.critic}</div>
-							<div className='grid-shadow-item grid-item-trickster'>{mbti.stack.trickster}</div>
-							<div className='grid-shadow-item grid-item-demon'>{mbti.stack.demon}</div>
-						</div>
-					}
-						<div className='grid-item grid-item-quadra'>{mbti.binaries.quadra}</div>
-						
-					</>
-				))}
-			</div>
-			<div className='card-items-hover'>
-				{mbti.filter(mbti => mbti.type.includes(pType)).map(mbti => (
-					<>
+		<motion.div className='card-container'
+			initial={{ '--card-height': '5.45vh' }}
+			whileHover={{ '--card-height': '9vh', scaleX: 1.03 }} 
+			transition={{ duration: 0.05 }}
+			onMouseEnter={() => setIsShown(true)}
+			onMouseLeave={() => setIsShown(false)}>
 
-					</>
-				))}
+			<div className='card-grid-summary'>
+				{cardSummary}
 			</div>
+
+			<AnimatePresence>
+				{(!isMobile && isShown) &&
+					<motion.div className='card-items-hover'
+						initial={{ y: -50, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						exit={{ y: -20, opacity: 0, }}>
+
+						{mbti.filter(mbti => mbti.type.includes(pType)).map(mbti => (
+							<>
+								asdf
+							</>
+						))}
+
+					</motion.div>
+				}
+			</AnimatePresence>
+
 			<div className='card-items-expanded'>
 				{mbti.filter(mbti => mbti.type.includes(pType)).map(mbti => (
 					<>
@@ -52,7 +73,8 @@ const Card = (props) => {
 					</>
 				))}
 			</div>
-		</div>
+
+		</motion.div>
 	</>
 	);
 };
